bb90e47ba56cbd1cd85d54b900279766436b3c28
YevheniiKyr <jekafekz@gmail.com>
2025-03-30 20:15:33 +0300

    return array from backend, fix regeneration logic

--- src/www_backend/main.mo
+++ src/www_backend/main.mo
@@ -3,31 +3,37 @@ import Blob "mo:base/Blob";
 import Text "mo:base/Text";
 
 actor Backend {
-    private stable var activityList = List.nil<(Text, Blob)>();
-    private stable var placeList = List.nil<(Text, Blob)>();
-    private stable var timeList = List.nil<(Text, Blob)>();
+    private stable var activityList: List.List<(Text,Text)> = List.nil();
+    private stable var placeList: List.List<(Text,Text)> = List.nil();
+    private stable var timeList : List.List<(Text,Text)> = List.nil();
 
-    public shared ({ caller }) func addActivity(name: Text, image: Blob) : async () {
-        activityList := List.push<(Text, Blob)>((name, image), activityList);
+    public shared ({ caller }) func addActivity(name: Text, image: Text) : async () {
+        activityList := List.push<(Text, Text)>((name, image), activityList);
     };
 
-    public shared ({ caller }) func addPlace(name: Text, image: Blob) : async () {
-        placeList := List.push<(Text, Blob)>((name, image), placeList);
+    public shared ({ caller }) func addPlace(name: Text, image: Text) : async () {
+        placeList := List.push<(Text, Text)>((name, image), placeList);
     };
 
-    public shared ({ caller }) func addTime(name: Text, image: Blob) : async () {
-        timeList := List.push<(Text, Blob)>((name, image), timeList);
+    public shared ({ caller }) func addTime(name: Text, image: Text) : async () {
+        timeList := List.push<(Text, Text)>((name, image), timeList);
     };
 
-    public query func getActivities() : async List.List<(Text, Blob)> {
-        activityList;
+    public query func getActivities() : async [(Text, Text)] {
+        List.toArray(activityList);
     };
 
-    public query func getPlaces() : async List.List<(Text, Blob)> {
-        placeList;
+    public query func getPlaces() : async [(Text, Text)] {
+        List.toArray(placeList);
     };
 
-    public query func getTimes() : async List.List<(Text, Blob)> {
-        timeList;
+    public query func getTimes() : async [(Text, Text)] {
+        List.toArray(timeList);
     };
+
+    public query func deleteAll() : async () {
+        activityList := List.nil();
+        placeList := List.nil();
+        timeList := List.nil();
+    }
 }
--- src/www_frontend/src/App.jsx
+++ src/www_frontend/src/App.jsx
@@ -7,75 +7,57 @@ export default function GeneratorApp() {
     const [activity, setActivity] = useState(null);
     const [place, setPlace] = useState(null);
     const [time, setTime] = useState(null);
-    const [images, setImages] = useState({activities: [], places: [], times: []});
-    const [url, setUrl] = useState("");
+    const [activities, setActivities] = useState([]);
+    const [times, setTimes] = useState([]);
+    const [places, setPlaces] = useState([]);
     const [loading, setLoading] = useState(true);
 
     async function fetchImages() {
         const activities = await actor.getActivities();
-        const blob = new Blob([new Uint8Array(activities[0][0][1])], { type: "image/jpg" })
-        console.log(activities[0][0][1].length)
-        console.log(new Uint8Array(activities[0][0][1]))
-        const url = URL.createObjectURL(blob)
-        setUrl(url);
         const places = await actor.getPlaces();
         const times = await actor.getTimes();
-        setImages({activities, places, times});
+        setActivities(activities);
+        setPlaces(places);
+        setTimes(times);
     }
 
-    const imageToBlob = async (imageUrl) => {
-        let uint8Array;
-        await fetch(imageUrl)
-            .then(response => response.arrayBuffer())
-            .then(buffer => {
-                uint8Array = new Uint8Array(buffer);
-            })
-            .catch(err => console.error("Error loading image:", err));
-        return uint8Array;
-    };
 
     async function initStartData() {
         let activities = [];
-        activities.push("tennis", "/tennis.jpg");
-        // activities.push("climbing", await fetchImageAsBlob("https://climbinghouse.com/wp-content/uploads/2023/06/woman_climbing_up_mountain-1.jpg"))
-        // activities.push("football", await fetchImageAsBlob("https://bsmedia.business-standard.com/_media/bs/img/article/2024-07/13/full/1720844888-869.jpg"))
-        // activities.push("restaurant", await fetchImageAsBlob("https://qul.imgix.net/b5dad46d-690e-4369-9ada-9cf86e6078c6/641947_sld.jpg"))
-        // activities.push("picnic", await fetchImageAsBlob("https://activeforlife.com/content/uploads/2018/08/family-picnic.jpg"))
-        for (let activity in activities) {
-            const file = await imageToBlob(activity[1])
-            console.log(file.length)
-            await actor.addActivity(activity[0], file);
+        activities.push(["tennis", "/tennis.jpg"]);
+        activities.push(["climbing", "/climbing.webp"])
+        activities.push(["football", "/football.jpg"])
+        activities.push(["restaurant", "/restaurant.jpg"])
+        activities.push(["picnic", "/family-picnic.webp"])
+        await actor.deleteAll();
+        for (let activity of activities) {
+            await actor.addActivity(activity[0], activity[1]);
         }
 
-        // let places = []
-        // places.push("Home city", await fetchImageAsBlob("https://thedailyguardian.com/wp-content/uploads/2024/04/544f82520cfdcf50f1d9132f05db2e1a.jpg"))
-        // places.push("Paris", await fetchImageAsBlob("https://img.static-af.com/transform/45cb9a13-b167-4842-8ea8-05d0cc7a4d04/"))
-        // places.push("London", await fetchImageAsBlob("https://railtour.ch/itineraries/destination-tours/London%20-%20Mit%20gutem%20Gewissen-95474/1098/image-thumb__1098__img-text-teaser/London%20-%20das%20gr%C3%BCne%20London%20entdecken-199835.541f8de9.jpg"))
-        // places.push("Prague", await fetchImageAsBlob("https://statemag.state.gov/wp-content/uploads/2023/10/1123pom-3-1.jpg"))
-        // places.push("Milano", await fetchImageAsBlob("hhttps://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Milan_Cathedral_from_Piazza_del_Duomo.jpg/800px-Milan_Cathedral_from_Piazza_del_Duomo.jpg"))
-        // for (let place in places) {
-        //     if (place[1]) {
-        //         const file = await imageToBlob(place[1])
-        //         await actor.addPlace(place[0], file);
-        //     }
-        // }
+        let places = []
+        places.push(["Home city", "/home.webp"])
+        places.push(["London", "/london.webp"])
+        places.push(["Prague", "/prague.webp"])
+        places.push(["Milano", "/milano.webp"])
+        for (let place of places) {
+            await actor.addPlace(place[0], place[1])
+        }
 
-        // let times = [];
-        // times.push("morning", await fetchImageAsBlob("https://d2jx2rerrg6sh3.cloudfront.net/images/news/ImageForNews_799838_17368258050432446.jpg"))
-        // times.push("day", await fetchImageAsBlob("https://images.photowall.com/products/44323/sunny-day.jpg?h=699&q=85"))
-        // times.push("evening", await fetchImageAsBlob("https://images.pexels.com/photos/221421/pexels-photo-221421.jpeg?cs=srgb&dl=pexels-pixabay-221421.jpg&fm=jpg"))
-        // times.push("night", await fetchImageAsBlob("https://static.scientificamerican.com/dam/m/57fc603972fdb/original/GettyImages-2166462891_WEB.jpg?m=1732209326.208"))
-        // for (let time in times) {
-        //     if (time[1]) {
-        //         await actor.addTime(time[0], time[1]);
-        //     }
-        // }
+        let times = [];
+        times.push(["morning", "morning.jpg"])
+        times.push(["day", "day.png"])
+        times.push(["evening", "evening.jpg"])
+        times.push(["night", "night.jpg"])
+        for (let time of times) {
+            await actor.addTime(time[0], time[1]);
+        }
     }
 
     useEffect(() => {
-        console.log("use effect")
         initStartData().then(() => {
-            fetchImages().then(() => {setLoading(false)});
+            fetchImages().then(() => {
+                setLoading(false)
+            });
         });
     }, []);
 
@@ -84,39 +66,47 @@ export default function GeneratorApp() {
     };
 
     const generateRandom = () => {
-        setActivity(getRandomItem(images.activities));
-        setPlace(getRandomItem(images.places));
-        setTime(getRandomItem(images.times));
+        const newActivity = getRandomItem(activities);
+        const newPlace = getRandomItem(places);
+        const newTime = getRandomItem(times);
+        console.log(newActivity);
+        console.log(newPlace);
+        console.log(newTime);
+        setActivity(newActivity);
+        setPlace(newPlace);
+        setTime(newTime);
     };
-    if(loading){
+
+    if (loading) {
         return <h1> Loading ... </h1>
     }
     return (
         <div className="d-flex flex-column align-items-center gap-3 p-3">
-            <Button onClick={generateRandom} variant="primary">Згенерувати</Button>
-            <Card.Img src={url} alt={"IMAGE"}/>
-            {/*{activity && place && time && (*/}
-            {/*    <div className="d-flex flex-wrap justify-content-center gap-3">*/}
-            {/*        <Card style={{width: "18rem"}}>*/}
-            {/*            <Card.Img variant="top" src={URL.createObjectURL(new Blob([activity[1]]))} alt={activity[0]}/>*/}
-            {/*            <Card.Body>*/}
-            {/*                <Card.Title className="text-center">{activity[0]}</Card.Title>*/}
-            {/*            </Card.Body>*/}
-            {/*        </Card>*/}
-            {/*        <Card style={{width: "18rem"}}>*/}
-            {/*            <Card.Img variant="top" src={URL.createObjectURL(new Blob([place[1]]))} alt={place[0]}/>*/}
-            {/*            <Card.Body>*/}
-            {/*                <Card.Title className="text-center">{place[0]}</Card.Title>*/}
-            {/*            </Card.Body>*/}
-            {/*        </Card>*/}
-            {/*        <Card style={{width: "18rem"}}>*/}
-            {/*            <Card.Img variant="top" src={URL.createObjectURL(new Blob([time[1]]))} alt={time[0]}/>*/}
-            {/*            <Card.Body>*/}
-            {/*                <Card.Title className="text-center">{time[0]}</Card.Title>*/}
-            {/*            </Card.Body>*/}
-            {/*        </Card>*/}
-            {/*    </div>*/}
-            {/*)}*/}
+            <h1>What Where When</h1>
+            <Button onClick={generateRandom} variant="primary">
+                Згенерувати
+            </Button>
+            <div className="d-flex flex-wrap justify-content-center gap-3">
+                <Card style={{width: "18rem"}}>
+                    <Card.Img variant="top" src={activity ? activity[1] : "unknown.png"}
+                              alt={activity ? activity[0] : "activity"}/>
+                    <Card.Body>
+                        <Card.Title className="text-center">{activity ? activity[0] : "Activity"}</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card style={{width: "18rem"}}>
+                    <Card.Img variant="top" src={place ? place[1] : "unknown.png"} alt={place ? place[0] : "place"}/>
+                    <Card.Body>
+                        <Card.Title className="text-center">{place ? place[0] : "Place"}</Card.Title>
+                    </Card.Body>
+                </Card>
+                <Card style={{width: "18rem"}}>
+                    <Card.Img variant="top" src={time ? time[1] : "unknown.png"} alt={time ? time[0] : "time"}/>
+                    <Card.Body>
+                        <Card.Title className="text-center">{time ? time[0] : "Time"}</Card.Title>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     );
 }
